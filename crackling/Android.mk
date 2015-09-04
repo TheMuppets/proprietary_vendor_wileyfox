af306008eebca72996d15fa54a22b0250a39a8e1
Ricardo Cerqueira <ricardo@cyngn.com>
2015-09-04 23:56:41 +0100

    crackling: Refresh blobs and add LiveDisplay libraries
    QC ref: 08438af8ae4b3e68af6553cafc657666a70bb618
    
    Change-Id: Ib1b5a664c2b5c9e01baa4d8adf0a33f6eea8ed99

--- crackling/Android.mk
+++ crackling/Android.mk
@@ -270,6 +270,20 @@ LOCAL_MULTILIB := 64
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libmm-disp-apis
+LOCAL_MODULE_OWNER := wileyfox
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libmm-disp-apis.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libmm-disp-apis.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH_32 := $(2ND_TARGET_OUT_VENDOR_SHARED_LIBRARIES)
+LOCAL_MULTILIB := both
+LOCAL_PROPRIETARY_MODULE := true
+include $(BUILD_PREBUILT)
+
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 
--- crackling/crackling-vendor.mk
+++ crackling/crackling-vendor.mk
@@ -29,7 +29,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libqct_resampler \
-    libmm-abl
+    libmm-abl \
+    libmm-disp-apis
 
 PRODUCT_PACKAGES += \
     libloc_api_v02 \
