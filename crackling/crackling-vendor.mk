1b37960a522ef49eb9c6f06859caf2f9cde16573
Ethan Chen <intervigil@gmail.com>
2016-03-02 17:03:12 -0800

    crackling: Remove QC location stack
    REF: CYNGNOS-1514

--- crackling/crackling-vendor.mk
+++ crackling/crackling-vendor.mk
@@ -16,9 +16,6 @@
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
-    com.qualcomm.location
-
-PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     shutdownlistener \
     TimeService
