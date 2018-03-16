35ab6b97e6b3d5fff5a714e9b1b396fe3a0b2e2a
Michael Bestas <mkbestas@lineageos.org>
2018-03-17 01:24:43 +0200

    crackling: Remove libwpa_qmi_eap_proxy
    Change-Id: I105a49d30720e66e7306f08e98cc2dc05ae59468

--- crackling/crackling-vendor.mk
+++ crackling/crackling-vendor.mk
@@ -1,5 +1,5 @@
 # Copyright (C) 2015-2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -272,7 +272,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libloc_api_v02 \
     libloc_ds_api \
-    libwpa_qmi_eap_proxy \
     libtime_genoff \
     TimeService \
     shutdownlistener \
