09a8a7a513b4adbed4a315cb197bb4fa86c1fa5e
Michael Bestas <mkbestas@lineageos.org>
2019-02-13 18:22:58 +0200

    kipper: Remove unused camera firmware
    Change-Id: I969f096d0132eff7126693fd3206b9b76442c733

--- kipper/kipper-vendor.mk
+++ kipper/kipper-vendor.mk
@@ -30,9 +30,6 @@ PRODUCT_COPY_FILES += \
     vendor/wileyfox/kipper/proprietary/vendor/bin/time_daemon:system/vendor/bin/time_daemon \
     vendor/wileyfox/kipper/proprietary/vendor/firmware/a420_pfp.fw:system/vendor/firmware/a420_pfp.fw \
     vendor/wileyfox/kipper/proprietary/vendor/firmware/a420_pm4.fw:system/vendor/firmware/a420_pm4.fw \
-    vendor/wileyfox/kipper/proprietary/vendor/firmware/cpp_firmware_v1_1_1.fw:system/vendor/firmware/cpp_firmware_v1_1_1.fw \
-    vendor/wileyfox/kipper/proprietary/vendor/firmware/cpp_firmware_v1_1_6.fw:system/vendor/firmware/cpp_firmware_v1_1_6.fw \
-    vendor/wileyfox/kipper/proprietary/vendor/firmware/cpp_firmware_v1_2_0.fw:system/vendor/firmware/cpp_firmware_v1_2_0.fw \
     vendor/wileyfox/kipper/proprietary/vendor/firmware/cpp_firmware_v1_4_0.fw:system/vendor/firmware/cpp_firmware_v1_4_0.fw \
     vendor/wileyfox/kipper/proprietary/vendor/firmware/venus.b00:system/vendor/firmware/venus.b00 \
     vendor/wileyfox/kipper/proprietary/vendor/firmware/venus.b01:system/vendor/firmware/venus.b01 \
